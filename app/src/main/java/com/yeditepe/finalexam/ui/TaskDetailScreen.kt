80737fe5eb051432f71a4d65fca31837450625fb
zeynepekmenn <zeynep.ekmen2@std.yeditepe.edu.tr>
2026-01-06 15:19:18 +0300

    Update TaskDetailScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
@@ -5,11 +5,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TaskDetailScreen(title: String) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         // TODO 4: Display task title
+        Text(
+            text = it.title,
+            fontSize = 20.sp
+                    fontWeight = FontWeight.Bold,
+            color = Color(0xFF333333)
+        )
+
+        )
     }
 }
