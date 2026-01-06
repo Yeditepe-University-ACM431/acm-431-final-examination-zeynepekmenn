23dd97196085210a61bf14e68586b34ee8409f44
zeynepekmenn <zeynep.ekmen2@std.yeditepe.edu.tr>
2026-01-06 15:20:44 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -6,8 +6,10 @@ class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
-
+private val_recipes = mutableStateOf<List<Task>>(emptyList())
+val task: State<List<Task>> = _tasks
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        task_taskList = task.isCompleted()
     }
 }
